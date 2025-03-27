786a271b3305a51e60a3a60e83e633f5f79b4aea
axel4j <kevinsbravo909@gmail.com>
2025-03-26 20:21:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Aquí puedes agregar funciones JavaScript si es necesario
+// Por ejemplo, para manejar eventos o interacciones
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página cargada correctamente.");
+});
